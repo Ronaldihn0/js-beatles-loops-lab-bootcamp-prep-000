28405382e939e8d3919f1cc631971e3622513e7e
Ronald DeBeras <rdeberas@gmail.com>
2018-03-09 21:49:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-function iLoveTheBeatles(n) {
-  var array = [];
-  var i = 0;
-  do {
-    i++
-    array.push("I love the Beatles!");
-  }
-  while (i<15)
-  return array
-}
+function theBeatlesPlay(musicians, instruments) {
+  const array = []
 
-for (let i = 0, n = musicians.length; i < n; i++) {
+  for (let i = 0, l = musicians.length; i < l; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 

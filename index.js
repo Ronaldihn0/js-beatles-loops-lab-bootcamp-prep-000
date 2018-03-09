836ab9abf0ca33b4a7fcc04c424ca2dab4e7947b
Ronald DeBeras <rdeberas@gmail.com>
2018-03-09 22:33:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ function iLoveTheBeatles(n) {
 
   return array
 }
+
+function johnLennonFacts(facts) {
+  const facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  while () {
+   facts[n] + "!!!";
+ }
+}
